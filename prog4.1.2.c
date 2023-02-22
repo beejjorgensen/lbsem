f247fbe6ebeae1addeb4352b44078a68116a3103
Brian "Beej Jorgensen" Hall <beej@beej.us>
2023-02-22 14:38:34 -0800

    sem_init

--- prog4.1.2.c
+++ prog4.1.2.c
@@ -11,12 +11,30 @@ sem_t *mutex, *items;
 const int pn = 20, cn = 50;  // Number of threads
 const int production_count = 10;
 
+int sem_init(sem_t *sem, int pshared, unsigned int value)
+{
+    (void)pshared;  // Always effectively `0`
+    const char const *name = "m
+
+    // Create the semaphore
+    if ((*sem = sem_open(name, O_CREAT, 0600, value)) == SEM_FAILED)
+        return -1;
+
+    // Unlink it so it will go away after this process exits
+    if (sem_unlink(name) == -1) {
+        sem_close(sem);
+        return -1;
+    }
+
+    return 0;
+}
+
 sem_t *sem_open_temp(const char *name, int value)
 {
     sem_t *sem;
 
     // Create the semaphore
-    if ((sem = sem_open(name, O_CREAT, 0666, value)) == SEM_FAILED)
+    if ((sem = sem_open(name, O_CREAT, 0600, value)) == SEM_FAILED)
         return SEM_FAILED;
 
     // Unlink it so it will go away after this process exits
